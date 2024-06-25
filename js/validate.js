17174448a7fbc2f8625183bfdd2ca527e47474d1
quandoan21-legion <qdoan21.legion@gmail.com>
2024-06-25 20:23:06 +0700

    sua file validate

--- js/validate.js
+++ js/validate.js
@@ -1,4 +1,12 @@
-function validateComment() {
-  let comment = document.getElementById("comment-form").value;
-  console.log(comment);
-}
+document.getElementById('commentForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var commentInput = document.getElementById('comment-form');
+  var commentValue = commentInput.value.trim();
+
+  if (commentValue !== '') {
+    alert('Comment text: ' + commentValue);
+  } else {
+    alert('Comment is empty!');
+  }
+});
